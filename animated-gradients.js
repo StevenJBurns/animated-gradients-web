d500f70e354ea471cf0efa8b81b6af92a8da615b
SJB <StevenJBurns@users.noreply.github.com>
2017-11-06 20:28:15 -0700

    Event handler added for button clicks; buttons added for all named colors

--- animated-gradients.js
+++ animated-gradients.js
@@ -1,7 +1,15 @@
 "use strict";
 
-$.getJSON("./named-colors.json", (obj) => {
-  console.log(obj);
+$.getJSON("named-colors.json", (colors) => {
+  for (let key in colors) {
+    let newButton = `<button class="color-button" data-color-hex="${colors[key]}">${key}</button>`;
+    $("#main-content").append(newButton);
+  };
+});
 
-
+$("#main-content").on("click", (e) => {
+  e.stopPropagation();
+  console.log($(e.target).data("color-hex"));
+  $("#main-content").css("background-color", $(e.target).data("color-hex"));
+  console.log(this);
 });
