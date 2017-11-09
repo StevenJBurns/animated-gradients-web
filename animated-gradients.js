6f52b71b1be29b903d70f7e19ab119c9a320f483
stevenjburns@msn.com <stevenjburns@msn.com>
2017-11-08 18:51:44 -0700

    Completed randomized starting color feature

--- animated-gradients.js
+++ animated-gradients.js
@@ -1,11 +1,19 @@
 "use strict";
 
-$.getJSON("named-colors.json", (colors) => {
-  for (let key in colors) {
-    let newButton = `<button class="color-button" data-color-hex="${colors[key]}">${key}</button>`;
-    $("#main-content").append(newButton);
-  };
-});
+$.getJSON("named-colors.json")
+  .then((colors) => {
+    for (let key in colors) {
+      let newButton = `<button class="color-button" data-color-hex="${colors[key]}">${key}</button>`;
+      $("#main-content").append(newButton);
+    }
+    return colors;
+  })
+  .then((colors) => {
+    let colorKeys = Object.keys(colors);
+    let randomizedIndex = Math.floor(Math.random() * (colorKeys.length));
+    let randomizedColor = colorKeys[randomizedIndex];    
+    $("html").css("background-color", `${[randomizedColor]}`);
+  });
 
 $("#main-content").on("click", (e) => {
   e.stopPropagation();
